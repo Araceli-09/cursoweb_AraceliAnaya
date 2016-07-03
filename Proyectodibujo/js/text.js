141b7d2bd0bfde5c5ef800d3f6611f24a189110d
ara anaya <ara anaya>
2016-07-02 21:43:35 -0500

    Mi noveno repositorio

---
+++ Proyectodibujo/js/text.js
@@ -0,0 +1,727 @@
+function comenzar(){
+var elemento= document.getElementById("micanvas");
+micanvas= elemento.getContext("2d");
+
+	micanvas.fillStyle ="#AFF800";
+ 	micanvas.beginPath();
+ 	micanvas.arc(750,300,80,0,Math.PI*2,true);
+ 	micanvas.closePath();
+ 	micanvas.lineWidth = 20;
+	micanvas.strokeStyle = "#AFF800";
+	micanvas.stroke();
+ 	micanvas.scale(2, 1);
+ 	micanvas.fill();
+
+  micanvas.fillStyle ="#0BAA16";
+	micanvas.save();
+	var radio = 50;
+  micanvas.scale(3, 3);
+	micanvas.beginPath();
+	micanvas.arc(70,100,50,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.restore();
+	micanvas.lineWidth = 10;
+	micanvas.strokeStyle = "#00921B";
+	micanvas.stroke();
+	micanvas.fill();
+
+	micanvas.beginPath();
+	micanvas.moveTo(290, 70);
+	micanvas.lineTo(300,174);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(290, 70);
+	micanvas.lineTo(200,95);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(200, 95);
+	micanvas.lineTo(259,115);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(259, 115);
+	micanvas.lineTo(265,155);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(329, 400);
+	micanvas.lineTo(330,550);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(330, 550);
+	micanvas.lineTo(250,505);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(250, 505);
+	micanvas.lineTo(290,480);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(290, 480);
+	micanvas.lineTo(290,432);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(100, 191);
+	micanvas.lineTo(70,150);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(70, 150);
+	micanvas.lineTo(30,155);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(30, 155);
+	micanvas.lineTo(75,95);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(75, 95);
+	micanvas.lineTo(145,160);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(113, 420);
+	micanvas.lineTo(80,470);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(80, 470);
+	micanvas.lineTo(40,450);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(40, 450);
+	micanvas.lineTo(100,530);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(100, 530);
+	micanvas.lineTo(150,442);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();	
+
+  micanvas.fillStyle ="black";
+  micanvas.beginPath();
+  micanvas.arc(406,355,5,0,Math.PI*2,true);
+  micanvas.closePath();
+  micanvas.fill();
+
+  micanvas.fillStyle ="black";
+  micanvas.beginPath();
+  micanvas.arc(400,233,5,0,Math.PI*2,true);
+  micanvas.closePath();
+  micanvas.fill();
+
+  micanvas.fillStyle ="white";
+  micanvas.beginPath();
+  micanvas.arc(406,355,2,0,Math.PI*2,true);
+  micanvas.closePath();
+  micanvas.fill();
+
+  micanvas.fillStyle ="white";
+  micanvas.beginPath();
+  micanvas.arc(400,233,2,0,Math.PI*2,true);
+  micanvas.closePath();
+  micanvas.fill();
+
+  micanvas.fillStyle ="#0BAA16";
+  micanvas.beginPath();
+  micanvas.arc(418,303,2,0,Math.PI*2,true);
+  micanvas.closePath();
+  micanvas.fill();
+
+  micanvas.fillStyle ="#0BAA16";
+  micanvas.beginPath();
+  micanvas.arc(417,280,2,0,Math.PI*2,true);
+  micanvas.closePath();
+  micanvas.fill();
+
+  micanvas.fillStyle ="#0BAA16";
+	micanvas.save();
+	var radio = 50;
+  micanvas.scale(3, 3);
+	micanvas.beginPath();
+	micanvas.arc(70,100,25,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.restore();
+	micanvas.lineWidth = 10;
+	micanvas.strokeStyle = "#00921B";
+	micanvas.stroke();
+	micanvas.fill();
+
+	micanvas.fillStyle ="#0BAA16";
+	micanvas.save();
+	var radio = 50;
+  micanvas.scale(3, 3);
+	micanvas.beginPath();
+	micanvas.arc(70,100,15,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.restore();
+	micanvas.lineWidth = 10;
+	micanvas.strokeStyle = "#00921B";
+	micanvas.stroke();
+	micanvas.fill();
+
+	micanvas.beginPath();
+	micanvas.moveTo(200, 379);
+	micanvas.lineTo(200,451);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(260, 364);
+	micanvas.lineTo(279,433);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(290, 310);
+	micanvas.lineTo(359,320);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(300, 179);
+	micanvas.lineTo(265,242);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(200, 220);
+	micanvas.lineTo(200,150);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(124, 177);
+  micanvas.lineTo(150,247);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(60, 300);
+  micanvas.lineTo(130,300);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(130, 428);
+  micanvas.lineTo(150,353);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(210,300,10,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+	micanvas.beginPath();
+	micanvas.moveTo(250, 320);
+  micanvas.lineTo(222,300);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(165, 300);
+  micanvas.lineTo(198,300);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(210, 288);
+  micanvas.lineTo(220,255);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(210,310);
+  micanvas.lineTo(210,345);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(180,230);
+  micanvas.lineTo(190,253);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(256,280);
+  micanvas.lineTo(275,260);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(144,335);
+  micanvas.lineTo(164,320);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(230,345);
+  micanvas.lineTo(238,370);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="3";
+	micanvas.stroke();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(306,200,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(322,225,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(335,252,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(345,282,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(310,287,18,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(297,250,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(260,210,15,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(225,200,15,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(225,168,18,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(265,177,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(180,175,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(146,185,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(165,215,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(115,215,20,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(100,250,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(80,280,18,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(120,275,18,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(130,350,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(90,350,19,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(120,388,19,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(110,320,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(158,400,19,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(183,425,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(220,425,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(260,425,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(246,390,19,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(300,340,18,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(300,387,25,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(335,340,17,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(205,240,10,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(230,240,12,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(257,257,12,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(270,287,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(270,320,12,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(250,350,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(218,360,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+  micanvas.beginPath();
+	micanvas.arc(190,360,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(165,345,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(148,305,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(150,280,10,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(159,260,10,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(175,250,10,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(190,320,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(226,324,15,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(235,287,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#00921B";
+	micanvas.beginPath();
+	micanvas.arc(193,280,16,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+	micanvas.beginPath();
+	micanvas.arc(120,160,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+	micanvas.beginPath();
+	micanvas.arc(95,160,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(95,135,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+	micanvas.beginPath();
+	micanvas.arc(75,115,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(55,140,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(280,140,15,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(280,100,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(250,100,10,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(280,500,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(305,500,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(305,470,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(305,440,13,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(318,527,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(100,500,14,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+  
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(100,465,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(123,465,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(118,440,11,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+  micanvas.fillStyle ="#AFF800";
+  micanvas.beginPath();
+	micanvas.arc(80,480,8,0,Math.PI*2,true);
+	micanvas.closePath();
+	micanvas.fill();
+
+	micanvas.beginPath();
+	micanvas.moveTo(30, 300);
+  micanvas.lineTo(60,280);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="10";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(30, 300);
+  micanvas.lineTo(60,300);
+	micanvas.strokeStyle ="#00921B";
+	micanvas.lineWidth ="10";
+	micanvas.stroke();
+
+	micanvas.beginPath();
+	micanvas.moveTo(30, 300);
+  micanvas.lineTo(56,293);
+	micanvas.strokeStyle ="#AFF800";
+	micanvas.lineWidth ="6";
+	micanvas.stroke();
+}
+
+
+window.addEventListener("load",comenzar,false);
